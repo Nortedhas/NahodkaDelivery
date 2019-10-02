a4cd7602dcc59b95b12f72e17268424135378423
Nortedhas <Nortedhas@gmail.com>
2019-10-02 11:29:56 +0300

    add MyOrderEmptyViewHolder

--- app/src/main/java/com/ageone/nahodka/Modules/MyOrder/MyOrderView.kt
+++ app/src/main/java/com/ageone/nahodka/Modules/MyOrder/MyOrderView.kt
@@ -4,11 +4,13 @@ import android.graphics.Color
 import android.view.ViewGroup
 import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.core.view.updatePadding
+import com.ageone.nahodka.Application.utils
 import com.ageone.nahodka.R
 import com.ageone.nahodka.External.Base.Module.BaseModule
 import com.ageone.nahodka.External.Base.RecyclerView.BaseAdapter
 import com.ageone.nahodka.External.Base.RecyclerView.BaseViewHolder
 import com.ageone.nahodka.External.InitModuleUI
+import com.ageone.nahodka.Modules.MyOrder.rows.MyOrderEmptyViewHolder
 import com.ageone.nahodka.Modules.MyOrder.rows.MyOrderTextViewHolder
 import com.ageone.nahodka.Modules.MyOrder.rows.initialize
 import yummypets.com.stevia.*
@@ -24,6 +26,7 @@ class MyOrderView(initModuleUI: InitModuleUI = InitModuleUI()) : BaseModule(init
 
     init {
 //        viewModel.loadRealmData()
+
 
         setBackgroundResource(R.drawable.back_white)//TODO: set background
 
@@ -51,14 +54,18 @@ class MyOrderView(initModuleUI: InitModuleUI = InitModuleUI()) : BaseModule(init
 
     inner class Factory(val rootModule: BaseModule) : BaseAdapter<BaseViewHolder>() {
 
-        private val MyOrderTextType = 0
+        private val MyOrderEmptyType = 0
 
-        override fun getItemCount() = 5//viewModel.realmData.size
+        private val MyOrderTextType = 1
 
-        override fun getItemViewType(position: Int): Int = when (position) {
+        override fun getItemCount() = 1//viewModel.realmData.size
 
-            else -> MyOrderTextType
-        }
+        override fun getItemViewType(position: Int): Int =
+          when(itemCount){
+              1 ->MyOrderEmptyType
+              else -> MyOrderTextType
+          }
+
 
         override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): BaseViewHolder {
 
@@ -69,6 +76,9 @@ class MyOrderView(initModuleUI: InitModuleUI = InitModuleUI()) : BaseModule(init
                 .height(wrapContent)
 
             val holder = when (viewType) {
+                MyOrderEmptyType -> {
+                    MyOrderEmptyViewHolder(layout)
+                }
                 MyOrderTextType -> {
                     MyOrderTextViewHolder(layout)
                 }
@@ -82,7 +92,13 @@ class MyOrderView(initModuleUI: InitModuleUI = InitModuleUI()) : BaseModule(init
 
         override fun onBindViewHolder(holder: BaseViewHolder, position: Int) {
 
+
             when (holder) {
+                is MyOrderEmptyViewHolder -> {
+                    setBackgroundColor(Color.parseColor("#eeece8"))
+                    holder.initialize("У Вас пока ещё нет \nсозданных заказов", R.drawable.dribbble,utils.variable.displayWidth)
+                }
+
                 is MyOrderTextViewHolder -> {
                     holder.initialize("24.02.19", "Находка", "ул. Темирязевская, д 155, кв. 210", "Tokio City", "1536.00")
                 }
---
+++ app/src/main/java/com/ageone/nahodka/Modules/MyOrder/rows/MyOrderEmptyViewHolder.kt
@@ -0,0 +1,61 @@
+package com.ageone.nahodka.Modules.MyOrder.rows
+
+import android.graphics.Color
+import androidx.constraintlayout.widget.ConstraintLayout
+import com.ageone.nahodka.Application.utils
+import com.ageone.nahodka.External.Base.ImageView.BaseImageView
+import com.ageone.nahodka.External.Base.RecyclerView.BaseViewHolder
+import com.ageone.nahodka.External.Base.TextView.BaseTextView
+import com.ageone.nahodka.External.Libraries.Glide.addImageFromGlide
+import yummypets.com.stevia.*
+
+class MyOrderEmptyViewHolder(val constraintLayout: ConstraintLayout) :
+    BaseViewHolder(constraintLayout) {
+
+    val textViewEmpty by lazy {
+        val textView = BaseTextView()
+        textView.textSize = 14F
+        textView.textColor = Color.BLACK
+        textView
+    }
+
+    val imageViewTurtle by lazy {
+        val imageView = BaseImageView()
+        imageView.initialize()
+        imageView
+    }
+
+    init {
+
+        renderUI()
+    }
+
+}
+
+fun MyOrderEmptyViewHolder.renderUI() {
+    constraintLayout.subviews(
+        imageViewTurtle,
+        textViewEmpty
+    )
+
+    constraintLayout.height(utils.variable.displayHeight)
+
+    textViewEmpty
+        .constrainTopToTopOf(constraintLayout,utils.variable.displayWidth / 3)
+        .constrainCenterXToCenterXOf(constraintLayout)
+
+    imageViewTurtle
+        .constrainTopToTopOf(textViewEmpty)
+        .constrainCenterXToCenterXOf(constraintLayout)
+
+}
+
+fun MyOrderEmptyViewHolder.initialize(text: String, image: Int,width: Int) {
+
+    imageViewTurtle
+        .width(width - 20)
+        .height(width * .75F)
+
+    textViewEmpty.text = text
+    addImageFromGlide(imageViewTurtle,image,0)
+}
