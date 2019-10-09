e57f081c82dc7c61c850662d303d2935eaf427c4
Katherina Yalanskaya <rinefica@gmail.com>
2019-10-09 12:16:26 +0300

    fix question module

--- app/src/main/java/com/ageone/nahodka/Modules/Question/rows/QuestionTextViewHolder.kt
+++ app/src/main/java/com/ageone/nahodka/Modules/Question/rows/QuestionTextViewHolder.kt
@@ -1,6 +1,8 @@
 package com.ageone.nahodka.Modules.Question.rows
 
 import android.graphics.Color
+import android.graphics.Typeface
+import android.view.Gravity
 import androidx.constraintlayout.widget.ConstraintLayout
 import com.ageone.nahodka.External.Base.RecyclerView.BaseViewHolder
 import com.ageone.nahodka.External.Base.TextView.BaseTextView
@@ -11,15 +13,21 @@ class QuestionTextViewHolder(val constraintLayout: ConstraintLayout) :
 
     val textViewContact by lazy {
         val textView = BaseTextView()
+        textView.gravity = Gravity.START
+        textView.typeface = Typeface.DEFAULT
         textView.textSize = 18F
         textView.textColor = Color.parseColor("#BFBFBF")
-        textView.text = "Если у Вас возникли какие то проблемы, можете обратиться в службу поддержки по номеру телефона: "
+        textView.setBackgroundColor(Color.TRANSPARENT)
         textView
     }
+
     val textViewNumber by lazy {
         val textView = BaseTextView()
+        textView.gravity = Gravity.START
+        textView.typeface = Typeface.DEFAULT
         textView.textSize = 18F
         textView.textColor = Color.parseColor("#818DFF")
+        textView.setBackgroundColor(Color.TRANSPARENT)
         textView
     }
 
@@ -37,16 +45,21 @@ fun QuestionTextViewHolder.renderUI() {
     )
 
     textViewContact
+        .fillHorizontally()
         .constrainTopToTopOf(constraintLayout,20)
-        .constrainLeftToLeftOf(constraintLayout,12)
+        .constrainLeftToLeftOf(constraintLayout,20)
+        .constrainRightToRightOf(constraintLayout, 25)
 
     textViewNumber
+        .fillHorizontally()
         .constrainTopToBottomOf(textViewContact,4)
-        .constrainLeftToLeftOf(constraintLayout,12)
+        .constrainLeftToLeftOf(constraintLayout,20)
+        .constrainRightToRightOf(constraintLayout, 25)
 
 }
 
 fun QuestionTextViewHolder.initialize(number: String) {
-
+    textViewContact.text =
+        "Если у Вас возникли какие-то проблемы, можете обратиться в службу поддержки по номеру телефона:"
     textViewNumber.text = number
 }
